8f3f3e942cf7a8ef77d5849e544764b236ca5ffb
vtadikonda <vtadikonda@vertafore.com>
2022-03-09 10:57:24 +0530

    Changed the text in the Feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Production Grade Docker Compose Workflow Version 1
+          Production Grade Docker Compose Workflow -Feature Branch
         </p>
         <a
           className="App-link"
